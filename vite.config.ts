5a57e1248b3957048c8ce044d460acf9fa741397
SyedWilayath <syedwilayath2@gmail.com>
2025-06-13 23:32:08 +0530

    git changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/wilayath-portfolio-project/", // Ensure correct base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
